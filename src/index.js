361a48ce2d949473c5d1080beb5cb285826ed9d6
Nikolas Burk <nikolas.burk@gmail.com>
2017-12-05 17:28:13 +0100

    use node-fetch instead of isomorphic-fetch and minor changes

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
-require('isomorphic-fetch')
+const fetch = require('node-fetch')
 const { makeRemoteExecutableSchema, introspectSchema } = require('graphql-tools')
 const { GraphQLServer } = require('graphql-yoga')
 const { createHttpLink } = require('apollo-link-http')
 
-const { USER_SERVICE_ID } = require('./services')
+const { DATABASE_SERVICE_ID } = require('./services')
 
 async function run() {
   // 1. Create Apollo Link that's connected to the underlying GraphQL API
-  const makeUserServiceLink = () => createHttpLink({
-    uri: `https://api.graph.cool/simple/v1/${USER_SERVICE_ID}`
+  const makeDatabaseServiceLink = () => createHttpLink({
+    uri: `https://api.graph.cool/simple/v1/${DATABASE_SERVICE_ID}`,
+    fetch
   })
 
   // 2. Retrieve schema definition of the underlying GraphQL API 
-  const userServiceSchemaDefinition = await introspectSchema(makeUserServiceLink())
+  const databaseServiceSchemaDefinition = await introspectSchema(makeDatabaseServiceLink())
 
   // 3. Create the executable schema based on schema definition and Apollo Link
-  const userServiceExecutableSchema = makeRemoteExecutableSchema({
-    schema: userServiceSchemaDefinition,
-    link: makeUserServiceLink()
+  const databaseServiceExecutableSchema = makeRemoteExecutableSchema({
+    schema: databaseServiceSchemaDefinition,
+    link: makeDatabaseServiceLink()
   })
   debugger
 
   // 4. Create and start proxy server based on the executable schema
-  const server = new GraphQLServer({ schema: userServiceExecutableSchema })
+  const server = new GraphQLServer({ schema: databaseServiceExecutableSchema })
   server.start(() => console.log('Server is running on http://localhost:4000'))
 }
 
